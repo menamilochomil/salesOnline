707e73e9b74764c2bf648d37a4c9dd59af1ae25b
AmeHSoc <hsocamerica@gmail.com>
2023-03-23 12:27:02 -0600

    test1

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
+    <h3>Holis</h3>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
